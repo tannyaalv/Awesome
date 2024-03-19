ec8bf52abe0f8272741af5c0405326c91ef2a5e1
Tanya Alvarez-Salsedo <tanyaalvsal@tamu.edu>
2024-03-19 02:38:41 -0500

    Initial Commit

---
+++ Awesome/AwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeApp.swift
+//  Awesome
+//
+//  Created by Alvarez-Salsedo, Tanya on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
